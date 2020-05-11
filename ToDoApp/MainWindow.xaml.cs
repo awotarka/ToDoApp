a280581ba94d519a218034c700c54864deb33df6
Vyacheslav Chekalin <awotarka@gmail.com>
2020-05-11 21:44:30 +0500

    Добавьте файлы проекта.

---
+++ ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoApp.models;
+using ToDoApp.Services;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<ToDoModel> _todoData;
+        private FileIOSepvice _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOSepvice(PATH);
+
+            try
+            {
+                _todoData = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+           
+            
+            dgTodoList.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoData_ListChanged;
+        }
+
+        private void _todoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType==ListChangedType.ItemAdded || e.ListChangedType==ListChangedType.ItemDeleted||e.ListChangedType==ListChangedType.ItemChanged)
+            {
+                try
+                {
+                   _fileIOService.SaveData(sender);     
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+    }
+}
---
+++ ToDoApp/models/ToDoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.models
+{
+    class ToDoModel : INotifyPropertyChanged
+    {
+        private bool _isDone;
+        private string _text;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value)
+                {
+                    return;
+                }
+                _isDone = value;
+                OnPropertyChanged("isDone");
+            }
+        }
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                {
+                    return;
+                }
+                _text = value;
+                OnPropertyChanged("_text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
